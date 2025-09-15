ff5272c2a528fd749f1f33550a6abd766c8c1826
Atul Kumar Rawat <118012606+atuuullll@users.noreply.github.com>
2025-09-15 17:49:52 +0000

    This is a for loop codes

---
+++ Syntax-refresh/loop/for.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=0;i<=5;i++){
+        cout<<i<<endl;
+    }
+
+    cout<<"New Code"<<endl;
+    cout<<"This is a series of even numbers"<<endl;
+    for(int i=0;i<=10;i=i+2){
+        cout<<i<<endl;
+    }
+}
