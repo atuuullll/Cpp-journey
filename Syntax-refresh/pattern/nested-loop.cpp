9f13b60fd6348d2292ab39ee3f519775cd8699ac
Atul Kumar Rawat <118012606+atuuullll@users.noreply.github.com>
2025-09-19 17:44:19 +0000

    add reverse triangular star code

--- Syntax-refresh/pattern/nested-loop.cpp
+++ Syntax-refresh/pattern/nested-loop.cpp
@@ -9,7 +9,14 @@ int main(){
     //     cout<<endl;
     // }
 
-    for(int i=1;i<=4;i++){
+    // for(int i=1;i<=4;i++){
+    //     for(int j=1;j<=i;j++){
+    //         cout<<" *";
+    //     }
+    //     cout<<endl;
+    // }
+
+    for(int i=4;i>=1;i--){
         for(int j=1;j<=i;j++){
             cout<<" *";
         }
