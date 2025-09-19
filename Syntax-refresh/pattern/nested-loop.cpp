1e3e879ffd3c34da9b43407b07a6de0aa824a590
Atul Kumar Rawat <118012606+atuuullll@users.noreply.github.com>
2025-09-19 17:36:19 +0000

    Add triangular star code

--- Syntax-refresh/pattern/nested-loop.cpp
+++ Syntax-refresh/pattern/nested-loop.cpp
@@ -1,11 +1,19 @@
 #include<iostream>
 using namespace std;
 int main(){
-    for(int i=1;i<=4;i++){
-        for(int j=1;j<=4;j++){
-            cout<<i<<" ";
+    // for(int i=1;i<=4;i++){
+    //     for(int j=1;j<=4;j++){
+    //         cout<<i<<" ";
 
+    //     }
+    //     cout<<endl;
+    // }
+
+    for(int i=1;i<=4;i++){
+        for(int j=1;j<=i;j++){
+            cout<<" *";
         }
         cout<<endl;
     }
 }
+
