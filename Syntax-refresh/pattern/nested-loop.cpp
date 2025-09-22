d146535c1ced4840d5c49211d05e2d06151e2614
Atul Kumar Rawat <118012606+atuuullll@users.noreply.github.com>
2025-09-22 17:40:56 +0000

    Add new pattern code problem solution

--- Syntax-refresh/pattern/nested-loop.cpp
+++ Syntax-refresh/pattern/nested-loop.cpp
@@ -46,19 +46,40 @@ int main(){
     // }
 
     //print hollow rectangle
-    int row,col;
-    cout<<"Enter the number of rows: ";
-    cin>>row;
-    cout<<"Enter the number of columns: ";
-    cin>>col;
-    for(int i=1;i<=row;i++){
-        for(int j=1;j<=col;j++){
-            if(i==1 || i==row || j==1 || j==col){
-                cout<<" *";
-            }
-            else{
-                cout<<"  ";
-            }
+    // int row,col;
+    // cout<<"Enter the number of rows: ";
+    // cin>>row;
+    // cout<<"Enter the number of columns: ";
+    // cin>>col;
+    // for(int i=1;i<=row;i++){
+    //     for(int j=1;j<=col;j++){
+    //         if(i==1 || i==row || j==1 || j==col){
+    //             cout<<" *";
+    //         }
+    //         else{
+    //             cout<<"  ";
+    //         }
+    //     }
+    //     cout<<endl;
+    // }
+
+    //print inverted and rotated half pyramid
+    // for(int i=1;i<=4;i++){
+    //     for(int j=1;j<=4-i;j++){
+    //         cout<<"  ";
+    //     }
+    //     for(int k=1;k<=i;k++){
+    //         cout<<" *";
+    //     }
+    //     cout<<endl;
+    // }
+    // return 0;
+
+    int n=4;
+    int num=1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<num++<<" ";
         }
         cout<<endl;
     }
