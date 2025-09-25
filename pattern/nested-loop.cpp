496d62b07a3050fa12e2d87da8f88fbf0ab6f5ee
atuuullll <atulkumarrawat74@gmail.com>
2025-09-25 18:17:46 +0000

    add new pattern code

--- pattern/nested-loop.cpp
+++ pattern/nested-loop.cpp
@@ -227,5 +227,21 @@ int main(){
     //     }
     //     cout<<endl;
     // }
+
+    //print the following pattern
+    // 1 2 3 4 5 
+    // 1 2 3 4 
+    // 1 2 3 
+    // 1 2 
+    // 1 
+    int n;
+    cout<<"Enter the number of rows: ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i+1;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
 }
 
