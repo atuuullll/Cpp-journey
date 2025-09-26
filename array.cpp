e922ff9a8e33331f49efa6982382ade4ca1a9374
atuuullll <atulkumarrawat74@gmail.com>
2025-09-26 17:43:32 +0000

    Add new code

--- array.cpp
+++ array.cpp
@@ -1,8 +1,31 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[5]={2,4,6,8,10};
-    for(int i=0;i<5;i++){
+    // int arr[5]={2,4,6,8,10};
+    // for(int i=0;i<5;i++){
+    //     cout<<arr[i]<<endl;
+    // }
+
+
+    // int n;
+    // cout<<"Enter the size of array: ";
+    // cin>>n;
+    // int arr[n]={2,4,6,8,10};
+    // for(int i=0;i<n;i++){
+    //     cout<<arr[i]<<endl;
+    // }
+    // return 0;
+
+    int n;
+    cout<<"Enter the size of array: ";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"The array elements are: "<<endl;
+    for(int i=0;i<n;i++){
         cout<<arr[i]<<endl;
     }
+    return 0;
 }
