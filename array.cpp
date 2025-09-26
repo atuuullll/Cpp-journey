64ef3e2aae74160406e010ce277adde3c06765a1
atuuullll <atulkumarrawat74@gmail.com>
2025-09-26 18:22:55 +0000

    Add new array code

--- array.cpp
+++ array.cpp
@@ -16,16 +16,26 @@ int main(){
     // }
     // return 0;
 
-    int n;
-    cout<<"Enter the size of array: ";
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
+    // int n;
+    // cout<<"Enter the size of array: ";
+    // cin>>n;
+    // int arr[n];
+    // for(int i=0;i<n;i++){
+    //     cin>>arr[i];
+    // }
+    // cout<<"The array elements are: "<<endl;
+    // for(int i=0;i<n;i++){
+    //     cout<<arr[i]<<endl;
+    // }
+    // return 0;
+
+    int arr[5]={2,4,6,8,10};
     cout<<"The array elements are: "<<endl;
-    for(int i=0;i<n;i++){
+    int i;
+    for(i=0;i<5;i++){
         cout<<arr[i]<<endl;
     }
+    cout<<"The size of array is: "<<sizeof(arr)<<endl;
+    cout<<"The number of elements in array is: "<<sizeof(arr)/sizeof(arr[0])<<endl;
     return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,8 +1,3 @@
-int n=4;
-    int num=1;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=i;j++){
-            cout<<num++<<" ";
-        }
-        cout<<endl;
+for(int i:arr){
+        cout<<i<<endl;
     }
