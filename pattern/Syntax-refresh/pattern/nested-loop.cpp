7e2c8ba94ffa212ed81ec879822cee512b9fca3c
atuuullll <atulkumarrawat74@gmail.com>
2025-09-26 17:44:37 +0000

    Add new code

--- pattern/Syntax-refresh/pattern/nested-loop.cpp
+++ pattern/Syntax-refresh/pattern/nested-loop.cpp
@@ -227,39 +227,5 @@ int main(){
     //     }
     //     cout<<endl;
     // }
-
-    //print the following pattern
-    // 1 2 3 4 5 
-    // 1 2 3 4 
-    // 1 2 3 
-    // 1 2 
-    // 1 
-    // int n;
-    // cout<<"Enter the number of rows: ";
-    // cin>>n;
-    // for(int i=1;i<=n;i++){
-    //     for(int j=1;j<=n-i+1;j++){
-    //         cout<<j<<" ";
-    //     }
-    //     cout<<endl;
-    // }
-
-
-    //print the following pattern
-    // 2 
-    // 3 4 
-    // 4 5 6 
-    // 5 6 7 8 
-    // 6 7 8 9 10 
-    // 7 8 9 10 11 12 
-    int n;
-    cout<<"Enter the number of rows: ";
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        for(int j=i+1;j<=i+i;j++){
-            cout<<j<<" ";
-        }
-        cout<<endl;
-    }
 }
 
