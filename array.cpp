e8013bbcd67fb094aa68c9bde112a6c5d13d5dd3
atuuullll <atulkumarrawat74@gmail.com>
2025-09-27 17:57:37 +0000

    Add new array code

--- array.cpp
+++ array.cpp
@@ -41,12 +41,20 @@ int main(){
 
 
     int arr[] = {2, 4, 8, 12, 16};
-    
-    // Accessing fourth element
+    cout<<"The array elements are: "<<endl;
+    int i;
+    for(i=0;i<sizeof(arr)/sizeof(arr[0]);i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    cout<<"Fourth element: ";
     cout << arr[3] << " ";
-    
-    // Accessing first element
+    cout<<endl;
+    cout<<"First element: ";
     cout << arr[0];
+    cout<<endl;
+    cout<<"Last element: ";
+    cout << arr[sizeof(arr)/sizeof(arr[0]) - 1];
     cout<<endl;
     
     return 0;
