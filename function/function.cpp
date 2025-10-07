ef87d5ac9ce889402063a1a371e65303cd91b6b1
atuuullll <atulkumarrawat74@gmail.com>
2025-10-07 15:32:27 +0000

    This is a parameter passing  function code

--- function/function.cpp
+++ function/function.cpp
@@ -1,10 +1,20 @@
 #include<iostream>
 using namespace std;
 
-void firstfunction(){
-    cout<<"This is my first function code"<<endl;
+// void firstfunction(){
+//     cout<<"This is my first function code"<<endl;
+// }
+// int main(){
+//     firstfunction();
+//     return 0;
+// }
+
+void secondfunction(int a, int b){
+    cout<<"Value of a is: "<<a<<endl;
+    cout<<"Value of b is: "<<b<<endl;
+    cout<<"The sum of a and b is: "<<a+b<<endl;
 }
 int main(){
-    firstfunction();
+    secondfunction(4,5);
     return 0;
 }
