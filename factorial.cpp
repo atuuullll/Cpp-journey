daaa7b005e9a8c659c6b2b5256c5097d71228b74
atuuullll <atulkumarrawat74@gmail.com>
2025-10-08 17:54:05 +0000

    Change of factorial code

--- factorial.cpp
+++ factorial.cpp
@@ -1,12 +1,14 @@
 #include<iostream>
 using namespace std;
 int factorial(int n){
-    int fact=1;for(int i=1; i<=n; i++){
+    int fact=1;
+    for(int i=1; i<=n; i++){
         fact=fact*i;
     }
     cout<<"Factorial of "<<n<<" is: "<<fact<<endl;
     return fact;
 }
+
 int main(){
     int num;
     cout<<"Enter a number: ";
