66acd0c0a1958ee54e1a3267696de953d4acdb75
atuuullll <atulkumarrawat74@gmail.com>
2025-10-29 23:57:22 +0530

    Add new input code

--- input.cpp
+++ input.cpp
@@ -8,5 +8,6 @@ int main(){
     cout<<"Next year, you will be "<<age+1<<" years old."<<endl;
     cout<<"Five years later, you will be "<<age+5<<" years old."<<endl;
     cout<<"Ten years later, you will be "<<age+10<<" years old."<<endl;
+    cout<<"Fifteen years later, you will be "<<age+15<<" years old."<<endl;
     return 0;
 }
