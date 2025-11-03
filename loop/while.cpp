ffb2409204c8a0e84727865d6d13404ad94bd0e2
Atul Kumar Rawat <118012606+atuuullll@users.noreply.github.com>
2025-11-03 18:10:35 +0000

    Add Hollow matrix code using while loop

--- loop/while.cpp
+++ loop/while.cpp
@@ -1,65 +1,86 @@
+// // #include<iostream>
+// // using namespace std;
+// // int main(){
+// //     int i=1;
+// //     while(i<=10){
+// //         cout<<i<<endl;
+// //         i++;
+// //     }
+
+// //     cout<<"New code"<<endl;
+// //     cout<<"This is a Fibonacci series"<<endl;
+
+// //     int a=0;
+// //     int b=1;
+// //     while(a<=10){
+// //         cout<<a<<endl;
+// //         int c=a+b;
+// //         a=b;
+// //         b=c;
+// //     }
+
+// //     cout<<"New code"<<endl;
+// //     cout<<"This is a multiplication table of 7"<<endl;
+
+// //     int n=7;
+// //     int j=1;
+// //     while(j<=10){
+// //         cout<<n*j<<endl;
+// //         j++;
+// //     }
+
+// //     cout<<"New code"<<endl;
+// //     cout<<"This is a series of odd numbers"<<endl;
+
+// //     int k=1;
+// //     while(k<=10){
+// //         cout<<k<<endl;
+// //         k=k+2;
+// //     }
+
+// //     cout<<"New code"<<endl;
+// //     cout<<"This is a series of even numbers"<<endl;
+
+// //     int x=0;
+// //     while(x<=10){
+// //         cout<<x<<endl;
+// //         x=x+2;
+// //     }
+// // }
+
 // #include<iostream>
 // using namespace std;
 // int main(){
+//     cout<<"Matrix of 3x3"<<endl;
 //     int i=1;
-//     while(i<=10){
-//         cout<<i<<endl;
+//     while(i<=3){
+//         int j=1;
+//         while(j<=3){
+//             cout<<"* ";
+//             j++;
+//         }   
+//         cout<<endl;
 //         i++;
 //     }
-
-//     cout<<"New code"<<endl;
-//     cout<<"This is a Fibonacci series"<<endl;
-
-//     int a=0;
-//     int b=1;
-//     while(a<=10){
-//         cout<<a<<endl;
-//         int c=a+b;
-//         a=b;
-//         b=c;
-//     }
-
-//     cout<<"New code"<<endl;
-//     cout<<"This is a multiplication table of 7"<<endl;
-
-//     int n=7;
-//     int j=1;
-//     while(j<=10){
-//         cout<<n*j<<endl;
-//         j++;
-//     }
-
-//     cout<<"New code"<<endl;
-//     cout<<"This is a series of odd numbers"<<endl;
-
-//     int k=1;
-//     while(k<=10){
-//         cout<<k<<endl;
-//         k=k+2;
-//     }
-
-//     cout<<"New code"<<endl;
-//     cout<<"This is a series of even numbers"<<endl;
-
-//     int x=0;
-//     while(x<=10){
-//         cout<<x<<endl;
-//         x=x+2;
-//     }
-// }
+// }   
 
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"Matrix of 3x3"<<endl;
-    int i=1;
-    while(i<=3){
+    cout<<"Hollow Matrix of 5x5"<<endl;
+    int i=1;    
+    while(i<=5){
         int j=1;
-        while(j<=3){
-            cout<<"* ";
+        while(j<=5){
+            if(i==1 || i==5 || j==1 || j==5){
+                cout<<"* ";
+            }
+            else{
+                cout<<"  ";
+            }
             j++;
-        }   
+        }
         cout<<endl;
         i++;
     }
-}   
+}
