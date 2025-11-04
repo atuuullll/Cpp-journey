c2ece3944a9c1c0f9d61b716a846585b600ca3a6
Atul Kumar Rawat <118012606+atuuullll@users.noreply.github.com>
2025-11-04 17:51:00 +0000

    Add right angle triangle code

--- loop/while.cpp
+++ loop/while.cpp
@@ -1,52 +1,52 @@
-// // #include<iostream>
-// // using namespace std;
-// // int main(){
-// //     int i=1;
-// //     while(i<=10){
-// //         cout<<i<<endl;
-// //         i++;
-// //     }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i=1;
+//     while(i<=10){
+//         cout<<i<<endl;
+//         i++;
+//     }
 
-// //     cout<<"New code"<<endl;
-// //     cout<<"This is a Fibonacci series"<<endl;
+//     cout<<"New code"<<endl;
+//     cout<<"This is a Fibonacci series"<<endl;
 
-// //     int a=0;
-// //     int b=1;
-// //     while(a<=10){
-// //         cout<<a<<endl;
-// //         int c=a+b;
-// //         a=b;
-// //         b=c;
-// //     }
+//     int a=0;
+//     int b=1;
+//     while(a<=10){
+//         cout<<a<<endl;
+//         int c=a+b;
+//         a=b;
+//         b=c;
+//     }
 
-// //     cout<<"New code"<<endl;
-// //     cout<<"This is a multiplication table of 7"<<endl;
+//     cout<<"New code"<<endl;
+//     cout<<"This is a multiplication table of 7"<<endl;
 
-// //     int n=7;
-// //     int j=1;
-// //     while(j<=10){
-// //         cout<<n*j<<endl;
-// //         j++;
-// //     }
+//     int n=7;
+//     int j=1;
+//     while(j<=10){
+//         cout<<n*j<<endl;
+//         j++;
+//     }
 
-// //     cout<<"New code"<<endl;
-// //     cout<<"This is a series of odd numbers"<<endl;
+//     cout<<"New code"<<endl;
+//     cout<<"This is a series of odd numbers"<<endl;
 
-// //     int k=1;
-// //     while(k<=10){
-// //         cout<<k<<endl;
-// //         k=k+2;
-// //     }
+//     int k=1;
+//     while(k<=10){
+//         cout<<k<<endl;
+//         k=k+2;
+//     }
 
-// //     cout<<"New code"<<endl;
-// //     cout<<"This is a series of even numbers"<<endl;
+//     cout<<"New code"<<endl;
+//     cout<<"This is a series of even numbers"<<endl;
 
-// //     int x=0;
-// //     while(x<=10){
-// //         cout<<x<<endl;
-// //         x=x+2;
-// //     }
-// // }
+//     int x=0;
+//     while(x<=10){
+//         cout<<x<<endl;
+//         x=x+2;
+//     }
+// }
 
 // #include<iostream>
 // using namespace std;
@@ -64,20 +64,36 @@
 //     }
 // }   
 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"Hollow Matrix of 5x5"<<endl;
+//     int i=1;    
+//     while(i<=5){
+//         int j=1;
+//         while(j<=5){
+//             if(i==1 || i==5 || j==1 || j==5){
+//                 cout<<"* ";
+//             }
+//             else{
+//                 cout<<"  ";
+//             }
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"Hollow Matrix of 5x5"<<endl;
-    int i=1;    
+    cout<<"Right Angled Triangle"<<endl;
+    int i=1;
     while(i<=5){
         int j=1;
-        while(j<=5){
-            if(i==1 || i==5 || j==1 || j==5){
-                cout<<"* ";
-            }
-            else{
-                cout<<"  ";
-            }
+        while(j<=i){
+            cout<<"* ";
             j++;
         }
         cout<<endl;
