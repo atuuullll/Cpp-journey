8ca91c69af79d4c3c94006d8cec2eb8da6e38f3c
Atul Kumar Rawat <118012606+atuuullll@users.noreply.github.com>
2025-11-05 16:03:56 +0000

    Add palindrome number code

--- rough.cpp
+++ rough.cpp
@@ -2,21 +2,40 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[] = {2, 4, 8, 12, 16};
-    cout<<"The array elements are: "<<endl;
-    int i;
-    for(i=0;i<sizeof(arr)/sizeof(arr[0]);i++){
-        cout<<arr[i]<<" ";
+    // int arr[] = {2, 4, 8, 12, 16};
+    // cout<<"The array elements are: "<<endl;
+    // int i;
+    // for(i=0;i<sizeof(arr)/sizeof(arr[0]);i++){
+    //     cout<<arr[i]<<" ";
+    // }
+    // cout<<endl;
+    // cout<<"Fourth element: ";
+    // cout << arr[3] << " ";
+    // cout<<endl;
+    // cout<<"First element: ";
+    // cout << arr[0];
+    // cout<<endl;
+    // cout<<"Last element: ";
+    // cout << arr[sizeof(arr)/sizeof(arr[0]) - 1];
+    // cout<<endl;
+    // return 0;
+
+    //palindrome number checking
+    int n,k,a,b;
+    cout<<"Enter a number: ";
+    cin>>n;
+    k=n;
+    a=0;
+    while(n>0){
+        b=n%10;
+        a=a*10+b;
+        n=n/10; 
     }
-    cout<<endl;
-    cout<<"Fourth element: ";
-    cout << arr[3] << " ";
-    cout<<endl;
-    cout<<"First element: ";
-    cout << arr[0];
-    cout<<endl;
-    cout<<"Last element: ";
-    cout << arr[sizeof(arr)/sizeof(arr[0]) - 1];
-    cout<<endl;
+    if(a==k){
+        cout<<"The number is a palindrome number"<<endl;
+    }
+    else{
+        cout<<"The number is not a palindrome number"<<endl;
+    }
     return 0;
 }   
