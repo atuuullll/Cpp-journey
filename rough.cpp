c18b5ba601b3749de8aa478b5d92e836ec3e75a9
Atul Kumar Rawat <118012606+atuuullll@users.noreply.github.com>
2025-11-08 18:06:17 +0000

    Add code for reverse array

--- rough.cpp
+++ rough.cpp
@@ -91,16 +91,31 @@ int main(){
     //}
 
     //missing number in an array
-    int arr[] = {1, 2, 4, 5, 6};
+    // int arr[] = {1, 2, 4, 5, 6};
+    // int n = sizeof(arr)/sizeof(arr[0]);
+    // int total = (n + 1) * (n + 2) / 2;
+    // int sum = 0;
+    // for(int i = 0; i < n; i++){
+    //     sum += arr[i];  
+    //     cout<<arr[i]<<" ";
+    // }
+    // int missing_number = total - sum;
+    // cout<<endl;
+    // cout<<"The missing number is: "<<missing_number<<endl;
+    // return 0;
+
+    // Reverse an array
+    int arr[] = {1, 2, 3, 4, 5};
     int n = sizeof(arr)/sizeof(arr[0]);
-    int total = (n + 1) * (n + 2) / 2;
-    int sum = 0;
+    cout<<"Original array: ";
     for(int i = 0; i < n; i++){
-        sum += arr[i];  
-        cout<<arr[i]<<" ";
+        cout<<arr[i]<<" ";  
     }
-    int missing_number = total - sum;
     cout<<endl;
-    cout<<"The missing number is: "<<missing_number<<endl;
+    cout<<"Reversed array: ";
+    for(int i = n - 1; i >= 0; i--){
+        cout<<arr[i]<<" ";  
+    }
+    cout<<endl;
     return 0;
 }
