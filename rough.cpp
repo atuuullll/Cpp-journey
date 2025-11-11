01cb8183149cd8659cfa14005c5c976dbd249bbc
Atul Kumar Rawat <118012606+atuuullll@users.noreply.github.com>
2025-11-11 18:41:54 +0000

    Add a code for printing Lower Pyramid of Diamond Pattern

--- rough.cpp
+++ rough.cpp
@@ -229,14 +229,29 @@ int main(){
     // return 0;
 
     //Print a upper pyramid of diamond pattern
+    // cout<<"Enter the value of n: ";
+    // int n;
+    // cin>>n;
+    // for(int i=1; i<=n; i++){
+    //     for(int j=i; j<n; j++){
+    //         cout<<" ";
+    //     }
+    //     for(int k=1; k<=(2*i-1); k++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+    // return 0;
+
+    // Print a lower pyramid of diamond pattern
     cout<<"Enter the value of n: ";
     int n;
     cin>>n;
-    for(int i=1; i<=n; i++){
+    for(int i=n; i>=1; i--){
         for(int j=i; j<n; j++){
             cout<<" ";
         }
-        for(int k=1; k<=(2*i-1); k++){
+        for(int k=1; k<=2*i-1; k++){
             cout<<"*";
         }
         cout<<endl;
