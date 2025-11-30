afd94280a041d3053f291dc72c215623b72b93a0
Atul Kumar Rawat <118012606+atuuullll@users.noreply.github.com>
2025-11-30 13:37:40 +0000

    Add code for printing all prime numbers up to n

--- Primenumber.cpp
+++ Primenumber.cpp
@@ -1,6 +1,35 @@
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
+// bool isPrime(int n){
+//     if(n==1){
+//         return false;
+//     }
+//     for(int i=2; i<=n-1; i++){
+//         if(n%i==0){
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+// int main(){
+//     int num;
+//     cout<<"Enter a number: ";
+//     cin>>num;
+//     if(isPrime(num)){
+//         cout<<num<<" is a prime number."<<endl;
+//     }
+//     else{
+//         cout<<num<<" is not a prime number."<<endl;
+//     }
+//     return 0;
+// }
+
+
+//print all prime numbers up to n
+#include <iostream>
+using namespace std;    
 bool isPrime(int n){
     if(n==1){
         return false;
@@ -11,17 +40,17 @@ bool isPrime(int n){
         }
     }
     return true;
-}
-
+}   
 int main(){
-    int num;
+    int n;
     cout<<"Enter a number: ";
-    cin>>num;
-    if(isPrime(num)){
-        cout<<num<<" is a prime number."<<endl;
+    cin>>n;
+    cout<<"Prime numbers up to "<<n<<" are: "<<endl;
+    for(int i=2; i<=n; i++){
+        if(isPrime(i)){
+            cout<<i<<" ";
+        }
     }
-    else{
-        cout<<num<<" is not a prime number."<<endl;
-    }
+    cout<<endl;
     return 0;
 }
