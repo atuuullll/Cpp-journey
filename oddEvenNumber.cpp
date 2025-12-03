20604e5eb79e3ef25a448de894a21c4844ce11a5
Atul Kumar Rawat <118012606+atuuullll@users.noreply.github.com>
2025-12-03 18:23:40 +0000

    Add code for printing odd even checker for an array

--- oddEvenNumber.cpp
+++ oddEvenNumber.cpp
@@ -17,24 +17,57 @@
 // }
 
 //print odd and even numbers up to n
+// #include <iostream>
+// using namespace std;
+// void oddEvenUpToN(int n){   
+//     cout<<"Odd numbers up to "<<n<<":"<<endl;
+//     for(int i=1; i<=n; i+=2){
+//         cout<<i<<" ";
+//     }
+//     cout<<endl;
+//     cout<<"Even numbers up to "<<n<<":"<<endl;
+//     for(int i=2; i<=n; i+=2){
+//         cout<<i<<" ";
+//     }
+//     cout<<endl;
+// }
+// int main(){
+//     int num;
+//     cout<<"Enter a number: ";
+//     cin>>num;
+//     oddEvenUpToN(num);
+//     return 0;
+// }
+
+
+//odd even checker for an array
 #include <iostream>
 using namespace std;
-void oddEvenUpToN(int n){   
-    cout<<"Odd numbers up to "<<n<<":"<<endl;
-    for(int i=1; i<=n; i+=2){
-        cout<<i<<" ";
+void oddEvenArray(int arr[], int size){
+    cout<<"Odd numbers in the array:"<<endl;
+    for(int i=0; i<size; i++){
+        if(arr[i]%2!=0){
+            cout<<arr[i]<<" ";
+        }
     }
     cout<<endl;
-    cout<<"Even numbers up to "<<n<<":"<<endl;
-    for(int i=2; i<=n; i+=2){
-        cout<<i<<" ";
+    cout<<"Even numbers in the array:"<<endl;
+    for(int i=0; i<size; i++){
+        if(arr[i]%2==0){
+            cout<<arr[i]<<" ";
+        }
     }
     cout<<endl;
 }
 int main(){
-    int num;
-    cout<<"Enter a number: ";
-    cin>>num;
-    oddEvenUpToN(num);
+    int n;
+    cout<<"Enter the size of the array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter "<<n<<" elements:"<<endl;
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    oddEvenArray(arr, n);
     return 0;
-}
+}   
