0f7c6239667b9999e54e0a04e416d48ad9cb8ded
Atul Kumar Rawat <118012606+atuuullll@users.noreply.github.com>
2025-12-03 18:21:43 +0000

    Add code for printing odd and even numbers up to n

--- oddEvenNumber.cpp
+++ oddEvenNumber.cpp
@@ -1,17 +1,40 @@
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+// void oddEven(int n){
+//     if(n%2==0){
+//         cout<<n<<" is an even number."<<endl;
+//     }
+//     else{
+//         cout<<n<<" is an odd number."<<endl;
+//     }
+// }
+// int main(){
+//     int a;
+//     cout<<"Enter a number: ";
+//     cin>>a;
+//     oddEven(a);
+//     return 0;
+// }
+
+//print odd and even numbers up to n
+#include <iostream>
 using namespace std;
-void oddEven(int n){
-    if(n%2==0){
-        cout<<n<<" is an even number."<<endl;
+void oddEvenUpToN(int n){   
+    cout<<"Odd numbers up to "<<n<<":"<<endl;
+    for(int i=1; i<=n; i+=2){
+        cout<<i<<" ";
     }
-    else{
-        cout<<n<<" is an odd number."<<endl;
+    cout<<endl;
+    cout<<"Even numbers up to "<<n<<":"<<endl;
+    for(int i=2; i<=n; i+=2){
+        cout<<i<<" ";
     }
+    cout<<endl;
 }
 int main(){
-    int a;
+    int num;
     cout<<"Enter a number: ";
-    cin>>a;
-    oddEven(a);
+    cin>>num;
+    oddEvenUpToN(num);
     return 0;
 }
