c2c608930dbe336cc8ffe82540051dcaa3c79d40
Atul Kumar Rawat <118012606+atuuullll@users.noreply.github.com>
2025-12-04 18:13:35 +0000

    Add code for Matrix Saddle Point

--- matrix.cpp
+++ matrix.cpp
@@ -194,41 +194,86 @@
 
 
 //Matrix Multiplication using functions
+// #include<iostream>
+// using namespace std;
+// void multiplyMatrices(int matrixA[2][2], int matrixB[2][2], int productMatrix[2][2]) {
+//     for(int i=0; i<2; i++){
+//         for(int j=0; j<2; j++){
+//             productMatrix[i][j] = 0;
+//             for(int k=0; k<2; k++){
+//                 productMatrix[i][j] += matrixA[i][k] * matrixB[k][j];
+//             }
+//         }   
+//     }
+// }
+// int main() {
+//     int matrixA[2][2], matrixB[2][2], productMatrix[2][2];  
+//     cout << "Enter elements of Matrix A (2x2):\n";
+//     for(int i=0; i<2; i++){
+//         for(int j=0; j<2; j++){
+//             cout << "Element [" << i + 1 << "][" << j + 1 << "]: ";
+//             cin >> matrixA[i][j];
+//         }
+//     }
+//     cout << "Enter elements of Matrix B (2x2):\n";
+//     for(int i=0; i<2; i++){
+//         for(int j=0; j<2; j++){
+//             cout << "Element [" << i + 1 << "][" << j + 1 << "]: ";
+//             cin >> matrixB[i][j];
+//         }
+//     }   
+//     multiplyMatrices(matrixA, matrixB, productMatrix);
+//     cout << "Product of Matrix A and Matrix B is:\n";
+//     for(int i=0; i<2; i++){
+//         for(int j=0; j<2; j++){
+//             cout << productMatrix[i][j] << "\t";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+
+//Matrix Saddle Point
 #include<iostream>
 using namespace std;
-void multiplyMatrices(int matrixA[2][2], int matrixB[2][2], int productMatrix[2][2]) {
-    for(int i=0; i<2; i++){
-        for(int j=0; j<2; j++){
-            productMatrix[i][j] = 0;
-            for(int k=0; k<2; k++){
-                productMatrix[i][j] += matrixA[i][k] * matrixB[k][j];
-            }
-        }   
-    }
-}
 int main() {
-    int matrixA[2][2], matrixB[2][2], productMatrix[2][2];  
-    cout << "Enter elements of Matrix A (2x2):\n";
-    for(int i=0; i<2; i++){
-        for(int j=0; j<2; j++){
-            cout << "Element [" << i + 1 << "][" << j + 1 << "]: ";
-            cin >> matrixA[i][j];
+    int matrix[10][10], n, saddlePoint = -1;    
+    cout << "Enter size of square matrix (n x n): ";
+    cin >> n;   
+    cout << "Enter elements of the matrix:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << "Element [" << i + 1 << "][" << j + 1
+                    << "]: ";       
+            cin >> matrix[i][j];
         }
-    }
-    cout << "Enter elements of Matrix B (2x2):\n";
-    for(int i=0; i<2; i++){
-        for(int j=0; j<2; j++){
-            cout << "Element [" << i + 1 << "][" << j + 1 << "]: ";
-            cin >> matrixB[i][j];
+    }    
+    for (int i = 0; i < n; i++) {
+        int minRow = matrix[i][0];
+        int colIndex = 0;
+        for (int j = 1; j < n; j++) {
+            if (matrix[i][j] < minRow) {
+                minRow = matrix[i][j];
+                colIndex = j;
+            }
+        }        
+        bool isSaddlePoint = true;  
+        for (int k = 0; k < n; k++) {
+            if (matrix[k][colIndex] > minRow) {
+                isSaddlePoint = false;
+                break;
+            }
+        }        
+        if (isSaddlePoint) {
+            saddlePoint = minRow;
+            break;
         }
-    }   
-    multiplyMatrices(matrixA, matrixB, productMatrix);
-    cout << "Product of Matrix A and Matrix B is:\n";
-    for(int i=0; i<2; i++){
-        for(int j=0; j<2; j++){
-            cout << productMatrix[i][j] << "\t";
-        }
-        cout << endl;
+    }    
+    if (saddlePoint != -1) {
+        cout << "Saddle Point is: " << saddlePoint << endl;
+    } else {
+        cout << "No Saddle Point found in the matrix." << endl;     
     }
     return 0;
 }
