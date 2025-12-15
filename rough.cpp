12c5af1e195f49dd405b3c2513a80b9c3b420e23
Atul Kumar Rawat <118012606+atuuullll@users.noreply.github.com>
2025-12-15 18:18:26 +0000

    Add code for a pattern to print a Hollow square

--- rough.cpp
+++ rough.cpp
@@ -705,17 +705,40 @@
 
 
 //pattern to print a Floyd's triangle
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n, num = 1;
+//     cout<<"Enter the value of n: ";
+//     cin>>n;
+//     for(int i=1; i<=n; i++){
+//         for(int j=1; j<=i; j++){
+//             cout<<num++<<" ";
+//         }       
+//         cout<<endl;
+//     }
+//     return 0;
+// }   
+
+
+//pattern to print a hollow square
 #include<iostream>
 using namespace std;
 int main(){
-    int n, num = 1;
+    int n;
     cout<<"Enter the value of n: ";
     cin>>n;
     for(int i=1; i<=n; i++){
-        for(int j=1; j<=i; j++){
-            cout<<num++<<" ";
-        }       
-        cout<<endl;
+        cout<<" * ";
+        for(int j=1; j<=n-1;j++){
+            if(i==1 || i==n){
+                cout<<" * ";
+            }
+            else {
+                cout<<"   ";
+            }
+        }
+        cout<<" * "<<endl;
     }
     return 0;
-}   
+}
