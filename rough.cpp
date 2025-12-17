5b9b2c6c2526aad8eb340d83271530252202e8fd
Atul Kumar Rawat <118012606+atuuullll@users.noreply.github.com>
2025-12-17 18:13:14 +0000

    Add code for patter to print Pascal's triangle

--- rough.cpp
+++ rough.cpp
@@ -765,29 +765,51 @@
 
 
 //pattern to print a full diamond
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter the value of n: ";
+//     cin>>n;
+//     for(int i=1; i<=n; i++){
+//         for(int j=i; j<n; j++){
+//             cout<<" ";
+//         }
+//         for(int k=1;k<=2*i-1; k++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+//     for(int i=n-1; i>=1; i--){
+//         for(int j=i; j<n; j++){
+//             cout<<" ";
+//         }
+//         for(int k=1; k<=2*i-1; k++){
+//             cout<<"*";
+//         }
+//         cout<<endl; 
+//     }
+//     return 0;
+// }
+
+
+//pattern to print Pascal's triangle
+#include <iostream>
 using namespace std;
-int main(){
+int main() {
     int n;
-    cout<<"Enter the value of n: ";
-    cin>>n;
-    for(int i=1; i<=n; i++){
-        for(int j=i; j<n; j++){
-            cout<<" ";
+    cout << "Enter the value of n: "; 
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int coeff = 1;
+        for (int j = 0; j < n - i - 1; j++) {
+            cout << " ";
+        }   
+        for (int k = 0; k <= i; k++) {
+            cout << coeff << " ";
+            coeff = coeff * (i - k) / (k + 1);
         }
-        for(int k=1;k<=2*i-1; k++){
-            cout<<"*";
-        }
-        cout<<endl;
-    }
-    for(int i=n-1; i>=1; i--){
-        for(int j=i; j<n; j++){
-            cout<<" ";
-        }
-        for(int k=1; k<=2*i-1; k++){
-            cout<<"*";
-        }
-        cout<<endl; 
+        cout << endl;
     }
     return 0;
 }
