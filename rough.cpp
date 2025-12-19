17a9decb431be3c457a04ab5f14ae68532485159
Atul Kumar Rawat <118012606+atuuullll@users.noreply.github.com>
2025-12-19 17:54:51 +0000

    Add code for function to check if a number is prime

--- rough.cpp
+++ rough.cpp
@@ -794,22 +794,45 @@
 
 
 //pattern to print Pascal's triangle
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int n;
+//     cout << "Enter the value of n: "; 
+//     cin >> n;
+//     for (int i = 0; i < n; i++) {
+//         int coeff = 1;
+//         for (int j = 0; j < n - i - 1; j++) {
+//             cout << " ";
+//         }   
+//         for (int k = 0; k <= i; k++) {
+//             cout << coeff << " ";
+//             coeff = coeff * (i - k) / (k + 1);
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+//function to check if a number is prime
+#include<iostream>
 using namespace std;
-int main() {
-    int n;
-    cout << "Enter the value of n: "; 
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        int coeff = 1;
-        for (int j = 0; j < n - i - 1; j++) {
-            cout << " ";
-        }   
-        for (int k = 0; k <= i; k++) {
-            cout << coeff << " ";
-            coeff = coeff * (i - k) / (k + 1);
+void checkPrime(int n){
+     for(int i=2; i<=n; i++){
+        if(n%i==0){
+            cout<<"The number is not prime"<<endl;
+            break;
         }
-        cout << endl;
+        else{
+            cout<<"The number is prime"<<endl;
+            break;
+        }
     }
-    return 0;
+}
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    checkPrime(n);
+    return 0;   
 }
