f7e9e66febd7caed16f75476f434cd1dff4af3bd
Atul Kumar Rawat <118012606+atuuullll@users.noreply.github.com>
2025-12-20 18:27:45 +0000

    Add code for factorial using recursion

--- factorial.cpp
+++ factorial.cpp
@@ -1,18 +1,37 @@
-#include<iostream>
-using namespace std;
-int factorial(int n){
-    int fact=1;
-    for(int i=1; i<=n; i++){
-        fact=fact*i;
-    }
-    cout<<"Factorial of "<<n<<" is: "<<fact<<endl;
-    return fact;
-}
+// #include<iostream>
+// using namespace std;
+// int factorial(int n){
+//     int fact=1;
+//     for(int i=1; i<=n; i++){
+//         fact=fact*i;
+//     }
+//     cout<<"Factorial of "<<n<<" is: "<<fact<<endl;
+//     return fact;
+// }
 
-int main(){
-    int num;
-    cout<<"Enter a number: ";
-    cin>>num;
-    factorial(num);
+// int main(){
+//     int num;
+//     cout<<"Enter a number: ";
+//     cin>>num;
+//     factorial(num);
+//     return 0;
+// }
+
+//factorial using recursion
+#include <iostream>
+using namespace std;        
+int factorial(int n) {
+    if (n <= 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}   
+int main() {
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+    int result = factorial(number);
+    cout << result << endl;
     return 0;
 }
