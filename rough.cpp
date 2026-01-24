c5f3817ef4db5b702cb989d4a65d74657a1ae90f
Atul Kumar Rawat <118012606+atuuullll@users.noreply.github.com>
2026-01-24 18:01:38 +0000

    Add code for printing a right angled triangle of numbers

--- rough.cpp
+++ rough.cpp
@@ -1013,17 +1013,33 @@
 // }
 
 //pattern to print a square of numbers
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int n;  
+//     cout << "Enter the value of n: "; 
+//     cin >> n;
+//     for (int i = 1; i <= n; i++) {
+//         for (int j = 1; j <= n; j++) {
+//             cout << i << " ";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+//pattern to print a right-angled triangle of numbers
+#include<iostream>
 using namespace std;
-int main() {
-    int n;  
-    cout << "Enter the value of n: "; 
-    cin >> n;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            cout << i << " ";
+int main(){
+    int n;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<i<<" ";
         }
-        cout << endl;
+        cout<<endl;
     }
     return 0;
 }
