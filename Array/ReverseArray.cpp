582e95a027e3ade3f1539b2521b14ee4867138a7
Atul Kumar Rawat <118012606+atuuullll@users.noreply.github.com>
2026-02-11 15:34:44 +0000

    Add code for Reverse an array without using extra space

--- Array/ReverseArray.cpp
+++ Array/ReverseArray.cpp
@@ -1,22 +1,51 @@
+//Reverse an array with extra space
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter the value of n: ";
+//     cin>>n;
+//     int arr[n];
+//     for(int i=0; i<n; i++){
+//         cout<<"Enter elements: "<<endl;
+//         cin>>arr[i];
+//     }
+//     cout<<"Original array: "<<endl;
+//     for(int i=0; i<n; i++){
+//         cout<<arr[i]<<" ";
+//     }
+//     cout<<endl;
+//     cout<<"Reversed array: "<<endl;
+//     for(int i=n-1; i>=0; i--){
+//         cout<<arr[i]<<" ";
+//     }
+//     cout<<endl;
+// }
+
+
+//Reverse an array without using extra space
 #include<iostream>
 using namespace std;
+void printArr(int *arr, int n){
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<",";
+    }
+    cout<<endl;
+}
+
 int main(){
-    int n;
-    cout<<"Enter the value of n: ";
-    cin>>n;
-    int arr[n];
-    for(int i=0; i<n; i++){
-        cout<<"Enter elements: "<<endl;
-        cin>>arr[i];
+    int arr[] = {1,2,3,4,5};
+    int n= sizeof(arr)/sizeof(int);
+    
+    int start=0, end=n-1;
+    while(start<end){
+        int temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+
+        start++;
+        end--;
     }
-    cout<<"Original array: "<<endl;
-    for(int i=0; i<n; i++){
-        cout<<arr[i]<<" ";
-    }
-    cout<<endl;
-    cout<<"Reversed array: "<<endl;
-    for(int i=n-1; i>=0; i--){
-        cout<<arr[i]<<" ";
-    }
-    cout<<endl;
+    printArr(arr,n);
+    return 0;
 }
